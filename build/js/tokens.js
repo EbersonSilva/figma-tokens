6c39f27e3bc71057337a9e3b3b18bd58fba18dea
EbersonSilva <EbersonSilva@users.noreply.github.com>
2022-10-20 02:27:59 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,11 +1,15 @@
 /**
  * Do not edit directly
- * Generated on Thu, 20 Oct 2022 01:35:16 GMT
+ * Generated on Thu, 20 Oct 2022 02:27:58 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorBlack": "#000000",
   "ColorWhite": "#ffffff",
+  "ColorPurple": "#971be3",
   "ColorGray100": "#c1c1c4",
   "ColorGray200": "#dedede",
   "ColorGray500": "#847c7c",
