0fddfe01389c6e9277c32f2c19c9905e4ca3be5a
Eberson Silva <65686722+EbersonSilva@users.noreply.github.com>
2022-10-19 22:48:12 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 20 Oct 2022 01:35:16 GMT
+ */
+
+module.exports = {
+  "ColorBlack": "#000000",
+  "ColorWhite": "#ffffff",
+  "ColorGray100": "#c1c1c4",
+  "ColorGray200": "#dedede",
+  "ColorGray500": "#847c7c",
+  "ColorGray800": "#222222",
+  "ColorGray900": "#121214",
+  "ColorBlue500": "#4361b4"
+};
